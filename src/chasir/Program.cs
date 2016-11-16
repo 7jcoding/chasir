0f34452cf05545f3a1d274dfd81cd196e3d2734e
linlvming <linlvming@linggan.com>
2016-11-16 10:09:13 +0800

    添加端口号命令行配置端启动程序

--- src/chasir/Program.cs
+++ src/chasir/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace chasir
 {
@@ -11,8 +12,20 @@ namespace chasir
     {
         public static void Main(string[] args)
         {
+            ///命令行配置端口号执行
+            ///dotnet chasir.dll --server.urls="http://localhost:6001;http://localhost:6002"
+            var host_config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+            //配置文件配置端口号执行
+            //var host_config = new ConfigurationBuilder()
+            //.SetBasePath(Directory.GetCurrentDirectory())
+            //.AddJsonFile("hosting.json", optional: true)
+            //.Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(host_config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
